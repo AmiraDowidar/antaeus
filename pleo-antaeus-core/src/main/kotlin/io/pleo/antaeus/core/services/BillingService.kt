0bf513795baab09be91c4f0c42b65bca20bb2567
Amira Dowidar <adowidar@newclassrooms.org>
2019-07-22 20:13:19 +0200

    Add missing reference and remove redundunt customerId

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,6 +1,10 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
+import io.pleo.antaeus.models.Transaction
+import io.pleo.antaeus.models.TransactionStatus
 
 class BillingService(
     private val paymentProvider: PaymentProvider
@@ -13,7 +17,7 @@ class BillingService(
             if (paid) {
                 return Transaction(invoice.copy(status = InvoiceStatus.PAID), true, "Success", TransactionStatus.AUTHORIZED)
             } else {
-                return Transaction(invoice, false, "Insufficient funds", TransactionStatus.FAILED)
+                return Transaction(invoice, false, "Insufficient funds", TransactionStatus.INSUFFICIENT_FUNDS)
             }
         }
         catch (e: Exception) {
--- pleo-antaeus-models/src/main/kotlin/io/pleo/antaeus/models/Transaction.kt
+++ pleo-antaeus-models/src/main/kotlin/io/pleo/antaeus/models/Transaction.kt
@@ -1,7 +1,6 @@
 package io.pleo.antaeus.models
 
 data class Transaction(
-    val customerID: Int,
     val invoice: Invoice,
     val accepted: Boolean,
     val description: String,
